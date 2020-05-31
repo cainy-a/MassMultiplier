77ee51f7f4fca768f5711e0fa546e3ac28381150
cainy-a <cainatkinson98@gmail.com>
2020-05-31 22:02:36 +0100

    Done

---
+++ MassMultiplier/MultiplicationSum.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace MassMultiplier
+{
+	public class MultiplicationSum
+	{
+		public MultiplicationSum(int firstNum, int lastNum)
+		{
+			FirstNum = firstNum;
+			LastNum = lastNum;
+		}
+
+		public int FirstNum;
+		public int LastNum;
+
+		public override string ToString()
+			=> $"{FirstNum} x {LastNum} = {FirstNum * LastNum}";
+
+		public int GetProduct() => FirstNum * LastNum;
+
+		public static int[] GetProducts(MultiplicationSum[] sums, out List<int> list)
+		{
+			var toReturn = new List<int>();
+			foreach (var sum in sums)
+			{
+				toReturn.Add(sum.GetProduct());
+			}
+
+			list = toReturn;
+			return toReturn.ToArray();
+		}
+	}
+}
---
+++ MassMultiplier/MultiplierWorker.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MassMultiplier
+{
+	public enum SaveModes { Full, Compact }
+	
+	public static class MultiplierWorker
+	{
+		public static List<MultiplicationSum> Main(string path, bool save = false, string savePath = null, SaveModes saveMode = SaveModes.Full)
+		{
+			var workingList = Multiply(path);
+			if (!save) return workingList;
+
+			var pathToSave = savePath ?? Path.Combine(Directory.GetCurrentDirectory(), "\\multiplied.txt");
+
+			var sw = File.CreateText(pathToSave);
+			var toWrite = string.Empty;
+			foreach (var sum in workingList)
+			{
+				if (saveMode == SaveModes.Full)
+					toWrite += $"{sum.ToString()}\n";
+				else
+					toWrite += $"{sum.GetProduct()}\n";
+			}
+			sw.Write(toWrite);
+			sw.Dispose();
+			return workingList;
+		}
+
+		private static List<MultiplicationSum> Multiply(string path)
+		{
+			if (!ValidatePath(path))
+				throw new FileNotFoundException("The file does not exist.", path);
+
+			string rawData;
+			var sr = new StreamReader(path);
+			rawData = Task.Factory.StartNew(() => sr.ReadToEndAsync()).Result.Result;
+			var multiplicationStrings = rawData.Split('\n').ToList();
+			var multiplicationSums = new List<MultiplicationSum>();
+			for (var i = 0; i < multiplicationStrings.Count; i++)
+			{
+				var sum = multiplicationStrings[i];
+				int num1;
+				int num2;
+				if (sum.Contains("x"))
+				{
+					num1 = Convert.ToInt32(sum.Split('x')[0]);
+					num2 = Convert.ToInt32(sum.Split('x')[1]);
+				}
+				else if (sum.Contains("*"))
+				{
+					num1 = Convert.ToInt32(sum.Split('*')[0]);
+					num2 = Convert.ToInt32(sum.Split('*')[1]);
+				}
+				else
+					throw new InvalidDataException($"Sum number {i} is in an incorrect format.");
+
+				multiplicationSums.Add(new MultiplicationSum(num1, num2));
+			}
+
+			return multiplicationSums;
+		}
+
+		public static bool ValidatePath(string path) => File.Exists(path);
+	}
+}
